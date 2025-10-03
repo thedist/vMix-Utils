fc18f8f420b701987fee04f12c3c2f69fa8ed8b3
Jeff Martin <dist@dist.dev>
2025-10-03 14:44:17 +0100

    Fix: OMTPreview support from latest beta

--- src/functions/omt.ts
+++ src/functions/omt.ts
@@ -1,8 +1,32 @@
 import { type VMixFunction } from '.'
 
-export type FunctionIDsOMT = 'OMTSelectSourceByIndex' | 'OMTSelectSourceByName'
+export type FunctionIDsOMT = 'OMTPreviewOff' | 'OMTPreviewOn' | 'OMTSelectSourceByIndex' | 'OMTSelectSourceByName'
 
 export const FunctionsOMT: VMixFunction[] = [
+  {
+    id: 'OMTPreviewOff',
+    category: 'OMT',
+    params: [
+      {
+        name: 'Input',
+        default: 'Preview',
+        required: false,
+      },
+    ],
+    version: '29',
+  },
+  {
+    id: 'OMTPreviewOn',
+    category: 'OMT',
+    params: [
+      {
+        name: 'Input',
+        default: 'Preview',
+        required: false,
+      },
+    ],
+    version: '29',
+  },
   {
     id: 'OMTSelectSourceByIndex',
     category: 'OMT',
